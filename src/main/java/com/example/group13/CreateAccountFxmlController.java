819309d6d2aae6083eba6948304f65e51708b9ae
asif001x <asifurrahman2341@gmail.com>
2025-04-18 21:18:12 +0600

    Merge remote-tracking branch 'origin/Asif' into Asif

---
+++ src/main/java/com/example/group13/CreateAccountFxmlController.java
@@ -0,0 +1,7 @@
+package com.example.group13;
+
+public class CreateAccountFxmlController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
--- src/main/java/com/example/group13/asifur2110384/MaintainController.java
+++ src/main/java/com/example/group13/asifur2110384/MaintainController.java
@@ -4,4 +4,5 @@ public class MaintainController
 {
     @javafx.fxml.FXML
     public void initialize() {
+        
     }}
