774f4895d256bece2b400f3eb78d54093cc217a5
Al-Rafi <alrafi980@gmail.com>
2025-04-18 23:22:27 +0600

    Changing controller

--- src/main/java/com/example/group13/rafi2231464/Bansecuritystaff.java
+++ src/main/java/com/example/group13/rafi2231464/Bansecuritystaff.java
@@ -1,7 +1,34 @@
 package com.example.group13.rafi2231464;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Bansecuritystaff
 {
     @javafx.fxml.FXML
+    private TableView tableTF;
+    @javafx.fxml.FXML
+    private TextField nameTF;
+    @javafx.fxml.FXML
+    private TableColumn nameTBC;
+    @javafx.fxml.FXML
+    private TableColumn statusTBC;
+    @javafx.fxml.FXML
+    private TableColumn idTBC;
+    @javafx.fxml.FXML
+    private TextField idTF;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void banOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOA(ActionEvent actionEvent) {
+    }
+}
