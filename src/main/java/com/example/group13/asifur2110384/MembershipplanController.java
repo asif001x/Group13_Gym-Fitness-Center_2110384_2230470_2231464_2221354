ec3a67db8c910f69c8f2a33875ff11a41e84ecb3
asif001x <asifurrahman2341@gmail.com>
2025-04-21 01:55:41 +0600

    Merge remote-tracking branch 'origin/Asif' into Asif

--- src/main/java/com/example/group13/asifur2110384/MembershipplanController.java
+++ src/main/java/com/example/group13/asifur2110384/MembershipplanController.java
@@ -1,7 +1,42 @@
 package com.example.group13.asifur2110384;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MembershipplanController
 {
     @javafx.fxml.FXML
+    private DatePicker dateDatePicker;
+    @javafx.fxml.FXML
+    private Label outputLabel;
+    @javafx.fxml.FXML
+    private TextField priceTextField;
+    @javafx.fxml.FXML
+    private TextField plannameTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void upcomingButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void currentButtonOnAction(ActionEvent actionEvent) {
+    }
+}
