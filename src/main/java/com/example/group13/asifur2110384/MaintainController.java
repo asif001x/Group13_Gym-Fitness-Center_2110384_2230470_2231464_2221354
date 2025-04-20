d1be9a425a04694eebed25706a7b6075c3b5dba9
asif001x <asifurrahman2341@gmail.com>
2025-04-21 01:22:48 +0600

    Merge remote-tracking branch 'origin/Asif' into Asif

--- src/main/java/com/example/group13/asifur2110384/MaintainController.java
+++ src/main/java/com/example/group13/asifur2110384/MaintainController.java
@@ -1,8 +1,19 @@
 package com.example.group13.asifur2110384;
 
+import javafx.event.ActionEvent;
+
 public class MaintainController
 {
     @javafx.fxml.FXML
     public void initialize() {
         
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+}
