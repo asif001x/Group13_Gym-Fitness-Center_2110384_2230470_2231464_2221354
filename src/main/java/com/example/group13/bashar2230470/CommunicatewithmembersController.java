fe5544c9af7e9dc966185421e695c72dac53b199
basha <basharjondir@gmail.com>
2025-04-21 15:41:56 +0600

    -----

--- src/main/java/com/example/group13/bashar2230470/CommunicatewithmembersController.java
+++ src/main/java/com/example/group13/bashar2230470/CommunicatewithmembersController.java
@@ -26,4 +26,7 @@ public class CommunicatewithmembersController
     @javafx.fxml.FXML
     public void backOnAction(ActionEvent actionEvent) {
     }
+
+    public void sendOnAction(javafx.event.ActionEvent actionEvent) {
+    }
 }
