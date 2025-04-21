da3bdfcf73683e23dea204da59d22332009aa753
basha <basharjondir@gmail.com>
2025-04-21 14:59:24 +0600

    add fxid

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.group13 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires jdk.jfr;
 
     opens com.example.group13 to javafx.fxml;
     exports com.example.group13;
