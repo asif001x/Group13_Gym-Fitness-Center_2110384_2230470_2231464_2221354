bfad87423ab1ff3bd172571441358144224beb8b
basha <basharjondir@gmail.com>
2025-04-21 15:39:08 +0600

    -----

--- src/main/java/com/example/group13/bashar2230470/AssignedgymmemberController.java
+++ src/main/java/com/example/group13/bashar2230470/AssignedgymmemberController.java
@@ -3,5 +3,13 @@ package com.example.group13.bashar2230470;
 public class AssignedgymmemberController
 {
     @javafx.fxml.FXML
+    private Label outputLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void refreshOnAction(ActionEvent actionEvent) {
+    }
+}
